ea6ebd9ea2dae53bbf63ec1fe828b137215e61b6
JasonJiangCN <jx240230@gmail.com>
2018-02-19 05:01:34 -0600

    p2a redirect done

--- p2a/wish.c
+++ p2a/wish.c
@@ -3,10 +3,12 @@
 #include<string.h>
 #include<stdlib.h>
 #include<sys/wait.h>
+#include<fcntl.h>
+//method declearations
+int execute(char* args[], char* retArgs[]);
 
 //path for binary files
 char* PATH[100];
-
 int command_cd(char* args[], int numArgs){
     if (numArgs != 2) {
         printf("Incorrect number of arguments for cd");
@@ -19,23 +21,48 @@ int command_cd(char* args[], int numArgs){
     return 0;
 }
 int command_path(char* args[], int numArgs){
-    int i = 1;
+    int i = 0;
     while(1){
-       if((PATH[i] = args[i]) == NULL)
-           break;
+        if((PATH[i] = args[i+1]) == NULL)
+            break;
         i++;
     } 
     return 0;
 }
+int lineSeperate(char* line, char* args[]) {
+    char *save;
+    int argsIndex = 0;
+    args[argsIndex++] = strtok_r(line, " ", &save);
+    while(1){
+        args[argsIndex] = strtok_r(NULL, " ",&save);
+        if (args[argsIndex] == NULL)
+            break;
+        argsIndex++;
+    }
+    return argsIndex;
+}
+int redirect(char* ret, char* line) {
+    char* progArgs[100];
+    char* retArgs[100];
+    ret[0] = '\0';
+    ret = ret + 2;
+    lineSeperate(line, progArgs);
+    int retArgc = lineSeperate(ret, retArgs);
+    if (retArgc != 1)
+        return 1;
+    execute(progArgs, retArgs);
+
+
+    return 0; 
+}
+
 int readCommand(char* args[]){
 
     //for user input
     char* line;
     size_t len = 0;
     ssize_t nread;
-    //for sperating the user input
-    int argsIndex = 0;
-
+    char* ret = NULL;
     if ((nread = getline(&line, &len, stdin)) == -1) {
         printf("Error reading line from user input");
         exit(1);
@@ -46,14 +73,14 @@ int readCommand(char* args[]){
     if (line[0] == EOF)
         exit(0);
 
-    char *save;
-    args[argsIndex++] = strtok_r(line, " ", &save);
-    while(1){
-        args[argsIndex] = strtok_r(NULL, " ",&save);
-        if (args[argsIndex] == NULL)
-            break;
-        argsIndex++;
+    //for redirection, 
+    if ((ret = strchr(line, '>'))){
+        redirect(ret, line);
+        return -1;
     }
+
+    //seperate the line
+    int argsIndex = lineSeperate(line, args);
     //exit if requested
     if (strcmp(args[0], "exit") == 0){
         exit(0);
@@ -62,31 +89,39 @@ int readCommand(char* args[]){
         command_cd(args, argsIndex);
         return -1;  //-1 for built-in command call flag
     }
-    if (strcmp(args[0]), "path" == 0){
-        command_cd(args, argsIndex);
+    if (strcmp(args[0], "path") == 0){
+        command_path(args, argsIndex);
         return -1;
     }
     return 0;
 }
-int execute(char* args[]){
+int execute(char* args[], char* retArgs[]){
     int pid;
     int status;
-    char* commandPath = NULL;
+    char commandPath[100];
     //test where is the expected executable file
-    char tempPath1[128] = "/bin/";
-    char tempPath2[128] = "/usr/bin/";
+    int i = 0; //index of PATH
     while(1) {
         if(PATH[i] == NULL)
             break;
         //copy the original string to a larger space
-        char* tempStr[100];
-        strcpy(tempStr, PATH[i]);
-        if (access(strcat(tempStr, args[0]), X_OK) == 0)
-            commandPath = tempStr;
+        char tempStr[100];
+        if (!strcpy(tempStr, PATH[i])){
+            printf("strcpy error\n");
+            return 1;
+        }
+        int strLen = strlen(tempStr);
+        tempStr[strLen] = '/';
+        tempStr[strLen + 1] = '\0';
+        strcat(tempStr, args[0]);
+        if (access(tempStr, X_OK) == 0){
+            strcpy(commandPath, tempStr);
+            break;
+        }
     } 
     if (commandPath == NULL) {
-        printf("Expected Executable File Cannot Be Found");
-        exit(1);
+        printf("Expected Executable File Cannot Be Found\n");
+        return 1;
     } 
     //fork and execute
     pid = fork();
@@ -95,8 +130,17 @@ int execute(char* args[]){
             perror("fork failed");
             exit(1);
         case 0:
+            if (retArgs){
+                int fd_out = open(retArgs[0],O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+                if (fd_out > 0){
+                    //redirect STDOUT for this process
+                    dup2(fd_out, STDOUT_FILENO);
+                    fflush(stdout);
+                }
+            }
             //the forked process will have a pid of 0 so it will execute  the file
-            execv(PATH, args);
+            execv(commandPath, args);
+
         default: 
             //for father process it will goes to default and wait
             waitpid(pid, &status, 0);
@@ -107,7 +151,7 @@ int main(int argc, char* argv[]){
 
     //char* userArgs[100] = &argv[1];
     if (argc != 1) {
-        
+
     }
 
     while(1) {
@@ -115,7 +159,8 @@ int main(int argc, char* argv[]){
         char* args[100];
         if (readCommand(args) == -1)
             continue;   //if a built-in command is called, continue;
-        execute(args);
+        if (execute(args, NULL) == 1)
+            continue;   //if there is an error, continue
 
     }
     return 0;
