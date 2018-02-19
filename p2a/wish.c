6035ed60b6cbf7753227987ef3ca8859fb8486e3
JasonJiangCN <jx240230@gmail.com>
2018-02-19 00:47:36 -0600

    milestone

--- p2a/wish.c
+++ p2a/wish.c
@@ -1,50 +1,91 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 
-char** readCommand(){
+//path for binary files
+char* PATH[100];
+
+int command_cd(char* args[], int numArgs){
+    if (numArgs != 2) {
+        printf("Incorrect number of arguments for cd");
+        exit(1);
+    }
+    if (chdir(args[1]) == -1){
+        printf("Error when trying to cd");
+        exit(1);
+    }
+    return 0;
+}
+int command_path(char* args[], int numArgs){
+    int i = 1;
+    while(1){
+       if((PATH[i] = args[i]) == NULL)
+           break;
+        i++;
+    } 
+    return 0;
+}
+int readCommand(char* args[]){
+
     //for user input
     char* line;
     size_t len = 0;
     ssize_t nread;
     //for sperating the user input
-    char* args[100];
-    char* sepPtr = NULL;
     int argsIndex = 0;
 
-    if ((nread = getline(&line, &len, stdin)) != -1) {
+    if ((nread = getline(&line, &len, stdin)) == -1) {
         printf("Error reading line from user input");
-        continue;
+        exit(1);
     }
-
-    //exit if requested
-    if (strcmp(line, "exit"))
-        exit(0);
+    //omit the last \n of the string
+    line[strlen(line) - 1] = '\0';
     //exit if EOF is read
     if (line[0] == EOF)
         exit(0);
 
-    args[argsIndex++] = strtok(line, " ");
+    char *save;
+    args[argsIndex++] = strtok_r(line, " ", &save);
     while(1){
-        args[argsIndex] = strtok(NULL, " ");
+        args[argsIndex] = strtok_r(NULL, " ",&save);
         if (args[argsIndex] == NULL)
             break;
         argsIndex++;
     }
-    return args;
+    //exit if requested
+    if (strcmp(args[0], "exit") == 0){
+        exit(0);
+    }
+    if (strcmp(args[0], "cd") == 0){
+        command_cd(args, argsIndex);
+        return -1;  //-1 for built-in command call flag
+    }
+    if (strcmp(args[0]), "path" == 0){
+        command_cd(args, argsIndex);
+        return -1;
+    }
+    return 0;
 }
-int pathSearch
 int execute(char* args[]){
     int pid;
     int status;
-    char* PATH;
+    char* commandPath = NULL;
     //test where is the expected executable file
-    if (access(strcat("/bin/", args[0]), X_OK) == 0)
-        PATH = strcat("/bin/", args[0]);
-    else if (access(strcat("/usr/bin/", args[0]), X_OK) == 0)
-        PATH = strcat("/usr/bin/", args[0]);
-    else {
-        printf("Expected Executable File Did Not Found");
+    char tempPath1[128] = "/bin/";
+    char tempPath2[128] = "/usr/bin/";
+    while(1) {
+        if(PATH[i] == NULL)
+            break;
+        //copy the original string to a larger space
+        char* tempStr[100];
+        strcpy(tempStr, PATH[i]);
+        if (access(strcat(tempStr, args[0]), X_OK) == 0)
+            commandPath = tempStr;
+    } 
+    if (commandPath == NULL) {
+        printf("Expected Executable File Cannot Be Found");
         exit(1);
     } 
     //fork and execute
@@ -58,22 +99,25 @@ int execute(char* args[]){
             execv(PATH, args);
         default: 
             //for father process it will goes to default and wait
-            while(wait(&status) != pid);
+            waitpid(pid, &status, 0);
     }
-
+    return 0;
 }
 int main(int argc, char* argv[]){
-    char* userArgs[argc - 1] = &argv[1];
-    if (argc != 1) {
 
+    //char* userArgs[100] = &argv[1];
+    if (argc != 1) {
+        
     }
 
     while(1) {
         printf("wish> ");
-        char** args = readCommand();
+        char* args[100];
+        if (readCommand(args) == -1)
+            continue;   //if a built-in command is called, continue;
         execute(args);
 
     }
-
+    return 0;
 }
 
