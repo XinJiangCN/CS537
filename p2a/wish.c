b76126aa3ccbf46544ee5aa24c4bd7da7b7dd55c
JasonJiangCN <jx240230@gmail.com>
2018-02-19 23:30:34 -0600

    p2a done

--- p2a/wish.c
+++ p2a/wish.c
@@ -4,11 +4,12 @@
 #include<stdlib.h>
 #include<sys/wait.h>
 #include<fcntl.h>
+#define DELIM " \t\r\n\a"
 //method declearations
 int execute(char* args[], char* retArgs[]);
 
 //path for binary files
-char* PATH[100];
+char* PATH[20];
 void error_handler(){
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message)); 
@@ -25,21 +26,21 @@ int command_cd(char* args[], int numArgs){
     return 0;
 }
 int command_path(char* args[], int numArgs){
-    int i = 1;
+    int i = 0;
     while(1){
-        if((PATH[i] = args[i]) == NULL)
+        if((PATH[i] = args[i+1]) == NULL)
             break;
         i++;
     } 
     return 0;
 }
-int lineSeperate(char* line, char* args[], char delim) {
+int lineSeperate(char* line, char* args[], char* delim) {
     char *save;
     int argsIndex = 0;
     //args = malloc(100*sizeof(char*));
     if (!args)
         args = malloc(100*sizeof(char));
-    args[argsIndex] = strtok_r(line, &delim, &save);
+    args[argsIndex] = strtok_r(line, delim, &save);
     argsIndex++;
     while(1){
         if (!(args + argsIndex)){
@@ -47,36 +48,48 @@ int lineSeperate(char* line, char* args[], char delim) {
             temp  = malloc(100*sizeof(char));
             temp++;
         }
-        args[argsIndex] = strtok_r(NULL, &delim,&save);
+        args[argsIndex] = strtok_r(NULL, delim,&save);
         if (args[argsIndex] == NULL)
             break;
         argsIndex++;
     }
+    if (args[0] == NULL)
+        return 0;
     return argsIndex;
 }
 int redirect(char* ret, char* line) {
     char* progArgs[100];
     char* retArgs[100];
     ret[0] = '\0';
-    ret = ret + 2;
-    lineSeperate(line, progArgs, ' ');
-    int retArgc = lineSeperate(ret, retArgs, ' ');
-    if (retArgc != 1)
+    ret = ret + 1;
+    int argsNum = lineSeperate(line, progArgs, DELIM);
+    if (argsNum == 0){
+        error_handler();
         return 1;
+    }
+    int retArgc = lineSeperate(ret, retArgs, DELIM);
+    if (retArgc != 1){
+        error_handler();
+        return 1;
+    }
     execute(progArgs, retArgs);
-
-
     return 0; 
 }
 int parallel(char* ret, char* line){
     char** commands = malloc(100*sizeof(char*));
-    int numCommands = lineSeperate(line, commands,'&');
-    char*** args = malloc(50*sizeof(char**));
+    int numCommands = lineSeperate(line, commands,"&");
+    char** args = malloc(50*sizeof(char*));
+    char* retRedir = malloc(100*sizeof(char));
     for (int i = 0; i < numCommands; i++) {
-        lineSeperate(commands[i], args[i], ' ');
-        execute(args[i], NULL);
+        if ((retRedir = strchr(commands[i], '>'))){
+            redirect(retRedir, commands[i]);
+            continue;
+        }
+        lineSeperate(commands[i], args, DELIM);
+        execute(args, NULL);
     }
-
+    free(args);
+    free(commands);
     return 0;
 }
 int readCommand(char* args[],FILE * fp){
@@ -88,8 +101,9 @@ int readCommand(char* args[],FILE * fp){
     char* retRedir = NULL;
     char* retParal = NULL;
 
+    fflush(stdin);
     if ((nread = getline(&line, &len, fp)) == -1) {
-        error_handler();
+        //error_handler();
         return 1;
     }
     if ((strcmp(line, "\n") == 0) || (strcmp(line, "") == 0))
@@ -98,23 +112,29 @@ int readCommand(char* args[],FILE * fp){
     if (line[strlen(line) - 1] == '\n')
         line[strlen(line) - 1] = '\0';
     //exit if EOF is read
-    if (line[0] == EOF)
+    if (line[0] == EOF){
         return 1;
-
-    //for redirection, 
-    if ((retRedir = strchr(line, '>'))){
-        redirect(retRedir, line);
-        return -1;
     }
     //forparallel commands
     if ((retParal = strchr(line, '&'))){
         parallel(retParal, line);
         return -1;
     }
+    //for redirection, 
+    if ((retRedir = strchr(line, '>'))){
+        redirect(retRedir, line);
+        return -1;
+    }
     //seperate the line
-    int argsIndex = lineSeperate(line, args, ' ');
+    int argsIndex = lineSeperate(line, args, DELIM);
+    if (argsIndex == 0) {
+        //error_handler();
+        return 0;
+    }
     //exit if requested
     if (strcmp(args[0], "exit") == 0){
+        if (args[1])
+            error_handler();
         exit(0);
     }
     if (strcmp(args[0], "cd") == 0){
@@ -133,6 +153,16 @@ int execute(char* args[], char* retArgs[]){
     char* commandPath = malloc(100*sizeof(char*));
     //test where is the expected executable file
     int i = 0; //index of PATH
+    int isNotFound = 1;
+    if (PATH[0] == NULL){
+        error_handler();
+        return 1;
+    }
+    if (args == NULL)
+        return 1;
+    if (args[0] == NULL)
+        return 1;
+
     while(1) {
         if(PATH[i] == NULL)
             break;
@@ -148,11 +178,14 @@ int execute(char* args[], char* retArgs[]){
         strcat(tempStr, args[0]);
         if (access(tempStr, X_OK) == 0){
             strcpy(commandPath, tempStr);
+            isNotFound = 0;
+            free(tempStr);
             break;
         }
         free(tempStr);
+        i++;
     } 
-    if (commandPath == NULL) {
+    if (isNotFound) {
         error_handler();
         return 1;
     } 
@@ -160,8 +193,9 @@ int execute(char* args[], char* retArgs[]){
     pid = fork();
     switch(pid){
         case -1:
+            printf("CNMLDG");
             error_handler();
-            exit(1);
+            return 1;
         case 0:
             if (retArgs){
                 int fd_out = open(retArgs[0],O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
@@ -193,17 +227,18 @@ int main(int argc, char* argv[]){
             exit(1);
         }
         isBatchMode = 1;
-    } else if (argc < 1 || argc > 3) {
+    } else if (argc < 1 || argc > 2) {
         error_handler();
         exit(1);
     }
     int isFinish = 0;
     while(1) {
-        
+
         if (isBatchMode == 1){
             while(1){
                 args = malloc(100*sizeof(char));
                 int readStatus = readCommand(args, fp);
+                fflush(fp);
                 if (readStatus == -1)
                     continue;
                 if (readStatus == 1) {
@@ -221,6 +256,7 @@ int main(int argc, char* argv[]){
             fflush(stdout);
             args = malloc(100*sizeof(char));
             int readStatus = readCommand(args, stdin);
+            fflush(stdin);
             if (readStatus == -1)
                 continue;   //if a built-in command is called, continue;
             if (readStatus == 1)
