017ce2b63f5321d61f47c8a16224843c47506601
JasonJiangCN <jx240230@gmail.com>
2018-03-19 00:14:11 -0500

    linux output bugs

--- p3/linux/pzip.c
+++ p3/linux/pzip.c
@@ -2,102 +2,93 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<sys/sysinfo.h>
-
+#include<string.h>
 pthread_mutex_t mutex;
 int NUM_THREADS;
 int done = 0;
-FILE *fp;
+FILE *fps[10];
 int file_num;
 int file_curr;
-char* file_names[10];
 
 char last = '\0';
 char curr = '\0';
-char output[1000];
+char *output;
 int output_index = 0;
-// Function declearation
-int open_file();
+int *count_output;
+
+int count = 1;
 void* zip(void* args){
-
-    pthread_mutex_lock(&mutex);
-    if (fp == NULL){
-
-        open_file();
-
-    }
-
-
-    char *line = NULL;
+    char *str = NULL;
     size_t len = 0;
     ssize_t nread;
+    int curr_file = 0;
 
-    int count = 1;
-    pthread_mutex_unlock(&mutex);
     while (1){
-        if ((nread = getline(&line, &len, fp)) == -1)
+        if(fps[curr_file] == NULL)
             break;
+
+        if((nread = getline(&str, &len, fps[curr_file])) == -1){
+            curr_file++;
+            continue;
+        }
         pthread_mutex_lock(&mutex);
-        if(fp == NULL)
-            break;
 
-
-        if (last == '\0' || curr != EOF)
-            last = *line;
+        if (str[nread - 1] == '\n')
+            str[nread - 1] = '\0';
+        // if (last == '\0' || curr != EOF)
+        //   last = *line;
         while (1){
-            if (*line == EOF || *line == '\0')
+            if(*str == '\0'){
+                count_output[output_index] = count;
+                output[output_index++] = last;
+                output[output_index] = '\0';
                 break;
-            curr = *line;
+            }
+            curr = *str;
+
             if (curr == last) {
                 // if the characters repeated
                 count += 1;
-            } else {
+            } else if(last != '\0'){
                 //print the number as well as the character
-                output[output_index++] = count + '0';
+                count_output[output_index] = count;
                 output[output_index++] = last;
                 output[output_index] = '\0';
                 count = 1;
             }
             last = curr;
-            line = line + 1;
+            str = str + 1;
         }
-
         pthread_mutex_unlock(&mutex);
-
-    }
-
-    if (curr == EOF){
-        fclose(fp);
-        fp = NULL;
+        
     }
     return NULL;
 }
-int open_file(){
 
-    if (file_curr <= file_num){
-        fp = fopen(file_names[file_curr], "r");    
-        if (fp == NULL){
-            printf("ERROR OPENING FILE");
-            exit(1);
-        }
-        file_curr++;
-    }
-
-
-    return 0;
-}
 int main(int argc, char* argv[]){
     if (argc == 1){
         printf("pzip: file1 [file2 ...] \n");
         exit(1); 
     } else {
+        //Initialization
         file_num = argc -1;
-        file_curr = 1;
-        for (int i = 1; i < argc + 1; i++){
-            file_names[i] = argv[i];
+        file_curr = 0;
+        for (int i = 0; i < argc -1; i++){
+            fps[i] = fopen(argv[i+1], "r");
+            if (fps[i] == NULL){
+                printf("ERROR OPENING FILE");
+                exit(1);
+            }
         }
         NUM_THREADS = get_nprocs();
         pthread_t threads[NUM_THREADS];
         pthread_mutex_init(&mutex, NULL);
+        output = malloc(1000*sizeof(char));
+        count_output = malloc(1000*sizeof(int));
+
+
+
+
 
         int rc;
         for (int i = 0; i < NUM_THREADS; i++){
@@ -112,7 +103,14 @@ int main(int argc, char* argv[]){
                 if(pthread_join(threads[j], NULL) != 0)
                     printf("ERROR WAITING \n");
         }
-        fwrite(output, 1, sizeof(output), stdout);
+        for (int k = 0; k < output_index; k++){
+            fwrite(count_output, sizeof(int), 1, stdout);
+            fwrite(output, sizeof(char), 1, stdout);
+            count_output = count_output +1;
+            output = output +1;
+        }
+        for(int i = 0; i < argc -1; i++)
+            fclose(fps[i]);
         pthread_exit(NULL);
     }
 }
