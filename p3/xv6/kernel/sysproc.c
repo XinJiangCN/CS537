052f343c6849c2ba4710d2fe0c207f52f755cc81
Jason <jason@Jasons-iMac.local>
2018-03-20 04:27:08 -0500

    xv6 final

---
+++ p3/xv6/kernel/sysproc.c
@@ -0,0 +1,129 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "proc.h"
+#include "sysfunc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return proc->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = proc->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+  
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(proc->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since boot.
+int
+sys_uptime(void)
+{
+  uint xticks;
+  
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int sys_mprotect(void)
+{
+    int addr;
+    int len;
+
+    if(argptr(0, (void*)&addr, sizeof(addr)) < 0)
+        return -1;
+    if(addr == 0)
+        return -1;
+    if(argint(1, &len) < 0)
+        return -1;
+    if(len <= 0)
+        return -1;
+    if(addr < PGSIZE || addr + 4 > proc->sz)
+        return -1;
+
+    return mprotect((void*)addr, len);
+}
+
+int sys_munprotect(void)
+{
+    int addr;
+    int len;
+
+    if(argptr(0, (void*)&addr, sizeof(addr)) < 0)
+        return -1;
+    if(addr == 0)
+        return -1;
+    if(argint(1, &len) < 0)
+        return -1;
+    if(len <= 0)
+        return -1;
+    if(addr < PGSIZE || addr + 4 > proc->sz)
+        return -1;
+
+    return munprotect((void*)addr, len);
+
+}
